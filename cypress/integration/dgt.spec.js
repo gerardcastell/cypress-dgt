6c89605868a8b01815d3690560809e922b009485
Gerard Castell <gerard.castell@cleverea.com>
2022-03-29 11:45:08 +0200

    [ADD] workaround to end test successfully

--- cypress/integration/dgt.spec.js
+++ cypress/integration/dgt.spec.js
@@ -6,6 +6,16 @@ describe('Should save changes in notifications tab', () => {
     cy.get('input[value="Vehículos"]').should('exist').click();
     cy.get('input[value="Matriculaciones"]').should('exist').click();
     cy.get('input[value="Microdatos"]').should('exist').click();
-    cy.get('input[value="Descargar"]').eq(1).should('exist').click();
+    cy.window()
+      .document()
+      .then(function (doc) {
+        doc.addEventListener('click', () => {
+          setTimeout(function () {
+            doc.location.reload();
+          }, 5000);
+        });
+        cy.get('input[value="Descargar"]').eq(1).should('exist').click();
+      });
+    // cy.get('input[value="Descargar"]').eq(1).should('exist').click();
   });
 });
